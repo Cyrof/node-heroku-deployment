2af84bb3aceb141772776b64d954e013b8ade22b
Cyrof <73649950+Cyrof@users.noreply.github.com>
2022-09-24 12:02:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Simple web server deployment from node to heroku
+
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3000
+
+// set the path to the html file
+
+// create function to use get method to issue the http get command
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'www/index.html'));
+});
+
+
+// create the listening function for the get request to port 8080
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
